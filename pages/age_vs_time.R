5c8fba1730625ac584f418d4f277eb336ee41522
Rebecca Klein <klein324@uw.edu>
2019-11-18 20:02:37 -0800

    sets up size/age user input widgets

--- pages/age_vs_time.R
+++ pages/age_vs_time.R
@@ -1,16 +1,36 @@
 age_vs_time_page <- tabPanel(
   "Size/Age Over Time",
-  titlePanel("Look at the Change in Salmon Size or Age Over Time"),
+  titlePanel("The Change in Salmon Size and Age Over Time"),
   
   sidebarLayout(
     # creates sidebar panel
     sidebarPanel(
-      h3("stuff")
-    ),
+      # can select either size or age to view data of
+      radioButtons("size_age",
+                   label = h3("Select Size or Age"),
+                   choices = list("Size" = 1, "Age" = 2), 
+                   selected = 1),
+      
+      # can select a river (or rivers) to look at data about
+      # none selected means you look at the whole data set
+      checkboxGroupInput("river",
+                         label = h3("Select river(s)"), 
+                         choices = list("Chilkat" = 1, "Stikine" = 2, "Taku" = 3, "Unuk" = 4),
+                         selected = NULL),
+      
+      # can change the range of years to look at the data
+      sliderInput("year_range",
+                  label = h3("Years"),
+                  min = 1988, 
+                  max = 2018,
+                  value = c(1988, 2018),
+                  sep = "") # means that there won't be a comma in the year
+      ),
     
     # creates main panel
     mainPanel(
-      h3("stuff")
+      h3("introduction to chart"),
+      h5("chart goes here")
     )
   )
 )
