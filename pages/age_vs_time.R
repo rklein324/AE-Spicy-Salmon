1f06c1babb3eee6571e8db428efd83445f73b822
Rebecca Klein <klein324@uw.edu>
2019-11-18 16:12:45 -0800

    add analysis folder

--- pages/age_vs_time.R
+++ pages/age_vs_time.R
@@ -1,6 +1,6 @@
 age_vs_time_page <- tabPanel(
-  "Age vs. Time",
-  titlePanel("Salmon Size & Age vs. Time"),
+  "Size/Age Over Time",
+  titlePanel("Look at the Change in Salmon Size or Age Over Time"),
   
   sidebarLayout(
     # creates sidebar panel
