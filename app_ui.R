66e8d76af2398b338f7b476941d28b362685ee56
Nahom Abi <nahomagidew@gmail.com>
2019-12-03 21:15:54 -0800

    shiny dashboard

--- app_ui.R
+++ app_ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinydashboard)
 
 source('pages/home.R')
 source('pages/size_vs_time.R')
@@ -10,12 +11,32 @@ source('pages/about.R')
 #-----------------------------------
 # Creates layout and puts together pages
 #-----------------------------------
-ui <- navbarPage(
-  "Shrinking Salmon",
-  home_page,
-  size_vs_time_page,
-  age_vs_size_page,
-  harvesting_page,
-  conclusion_page,
-  about_page
+ui <- dashboardPage(
+  dashboardHeader(title = "Shrinking Salmon"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Size vs. Time", tabName = "size_vs_time", icon = icon("clock")),
+      menuItem("Size vs. Age", tabName = "age_vs_size", icon = icon("stream")),
+      menuItem("Harvesting", tabName = "harvesting", icon = icon("fish")),
+      menuItem("Conclusion", tabName = "conclusion", icon = icon("hourglass-end")),
+      menuItem("About", tabName = "about", icon = icon("address-card"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "home",
+              home_page),
+      tabItem(tabName = "size_vs_time",
+              size_vs_time_page),
+      tabItem(tabName = "age_vs_size",
+              age_vs_size_page),
+      tabItem(tabName = "harvesting",
+              harvesting_page),
+      tabItem(tabName = "conclusion",
+              conclusion_page),
+      tabItem(tabName = "about",
+              about_page)
+    )
+  )
 )
