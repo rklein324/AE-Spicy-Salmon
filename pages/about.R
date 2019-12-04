e332f87dfd51212736fe660fc842532d11000443
Nahom Abi <nahomagidew@gmail.com>
2019-12-03 20:14:52 -0800

    Minor changes to about layout.

--- pages/about.R
+++ pages/about.R
@@ -3,11 +3,11 @@ about_page <- tabPanel(
   titlePanel("About Us"),
   
   flowLayout(
-      p("Aarushi Bhatnagar"),
+      p(h3("Aarushi Bhatnagar")),
       p(h3("Nahom Abi"), "Nahom is a junior at the University of Washington
         pursuing a degree in Informatics. He plans to start a company after
         graduation."),
-      p("Nitidh Lamsam"),
+      p(h3("Nitidh Lamsam")),
       p(h3("Rebecca Klein"), "Rebecca is a junior at the University of
         Washington and an Informatics major. She transfered to UW this year
         from North Seattle College. Rebecca aspires to be a data scientist
