e0a373ee7fb2457e4fab570e7847fae3c9b244bd
Rebecca Klein <klein324@uw.edu>
2019-12-03 22:21:54 -0800

    fix mistake in previous merge conflict

--- pages/age_vs_size.R
+++ pages/age_vs_size.R
@@ -37,8 +37,9 @@ age_vs_size_page <- tabPanel(
     mainPanel(
       p("This page shows the age versus size of the salmon.",
         "In the sidebar window, the user can filter the sex of the salmon",
-<<<<<<< HEAD
-        "and at the same time select the river."),
+        "and at the same time select the river. By default, the widget selects 
+        'All'for the gender (with radio buttons) and selects all four rivers
+        (using checkboxes)."),
 
       p("The chart shows the change in salmon size as their age develop.",
         "We can clearly see that there is something wrong with the female at",
@@ -51,16 +52,6 @@ age_vs_size_page <- tabPanel(
         1988-2017."),
 
 
-      plotlyOutput(outputId = "age_vs_size")
-=======
-        "and at the same time select the river. By default, the widget selects 
-        'All'for the gender (with radio buttons) and selects all four rivers
-        (using checkboxes)."), 
-      
-      p("This information is obtained from Metadata: Chinook salmon",
-        "age, sex, and length data from major rivers in Southeast Alaska, 1988-2017."), 
-
-     
       plotlyOutput(outputId = "age_vs_size"),
       
       p("The chart shows the salmon's change in size as their age develop.",
@@ -69,8 +60,6 @@ age_vs_size_page <- tabPanel(
         "We had to calculate the average size for each age because age is", 
         "numerically discrete, so we cannot have a scatter plot. The average size", 
         "allows us to obtain one result for each age inorder to format a line graph."),
-        
-      
         "Chilkat is the only river that has higher average size of salmon",
         "at the age 2 compared to 3 (for both male and female).",
         "We can clearly see that there is something wrong for the female at",
@@ -79,7 +68,6 @@ age_vs_size_page <- tabPanel(
         "looks like what we expected. Overall, the average size in male and female",
         "are very similar for all ages apart from the anomaly (age = 2 in the",
         "Chilkat river)."),
->>>>>>> 5df22b8122cb9f18907d8b66e097f5a693bd9918
     )
   )
 
